861c4acc17790bf9c5e87cac546538cdd18532d6
E Yeung <ernestyalumni@gmail.com>
2015-09-01 03:02:58 -0700

    Update firms_by_NASDAQ.py

--- firms_by_NASDAQ.py
+++ firms_by_NASDAQ.py
@@ -51,7 +51,7 @@ import collections
 
 iamauser = raw_input("Input your username: ")
 if iamauser == "":
-    iamauser = "ernestyeung"
+    iamauser = ""
 password = raw_input("Input your password: ")
 
 engine = create_engine("postgresql://"+iamauser+":"+password+"@localhost/marche")
